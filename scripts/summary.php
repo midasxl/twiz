cfe41d3b67b89661d9bfbb918bbc611a618c11f4
Mark Husband-Wood <sparkhw@gmail.com>
2018-02-17 15:51:27 -0500

    new snippets examples

--- scripts/summary.php
+++ scripts/summary.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -85,6 +85,8 @@ foreach ($_POST as $key => $value) {
     echo "</td>";
     echo "</tr>";
 }
+
+echo '<div id="stdDev"></div>';
 
 $source = '../../uploads/'.$_POST["card"];
 $xmldata = simplexml_load_file($source);
@@ -1232,6 +1234,7 @@ $(document).ready(function(){
 
 <script>
 $(document).ready(function(){
+
 	$(".hideRows").click(function(e){e.preventDefault();
 		var target = $(this).prop('id');
 		target = '.' + target;
@@ -1241,6 +1244,7 @@ $(document).ready(function(){
 		   };
 		});
 	});
+
 	$(".restoreRows").click(function(e){e.preventDefault();
 		var target = $(this).prop('id');
 		target = '.' + target;
@@ -1250,15 +1254,75 @@ $(document).ready(function(){
 		   };
 		});
 	});
-			$('#printMe').click(function() {
-				window.print();
-				return false;
-			});
-			$('#showlegend').click(function(e){
-				e.preventDefault();
-				$('#legend').slideToggle('slow');
-			});
+
+	$('#printMe').click(function() {
+		window.print();
+		return false;
+	});
+
+	$('#showlegend').click(function(e){
+		e.preventDefault();
+		$('#legend').slideToggle('slow');
+	});
+
+  (function (){
+      var TableData;
+      TableData = storeTblValues();
+      //convert array of strings to an array of integers
+      var arrayOfNumbers = TableData.map(Number);
+      var stdDev;
+      stdDev = standardDeviation(arrayOfNumbers);//send array to standardDeviation function
+      $("#stdDev").text(stdDev);//output sd to div on page
+  })();
+
+  function storeTblValues(){
+      var TableData = new Array();
+      $('#DataTables_Table_0 tr').each(function(row, tr){
+          TableData[row]=$(tr).find('td:eq(17)').text();
+      });
+      TableData.shift();  // first row will be empty - so remove
+      return TableData;
+  }
+
+  // standard deviation logic
+
+  /*$(function(){
+    var stdDev = standardDeviation([1, 2, 3, 4, 5, 6, 7, 8, 9, 25]);
+    $("#stdDev").text(stdDev);
+  });*/
+
+  function standardDeviation(values){
+      // get the average of the data set
+      var avg = average(values); // call average function
+      // calculate difference between each array value and the average value
+      // map function will iterate over array items and return a new array of these values
+      var squareDiffs = values.map(function(value){
+        var diff = value - avg;
+        // square the differences of each value in this step as well
+        var sqrDiff = diff * diff;
+        return sqrDiff;
+      });
+      // get the average of the squared differences
+      var avgSquareDiff = average(squareDiffs); // call average function
+      // calculate the square root of the average squared differences
+      // final result is the standard deviation for the data set
+      var stdDev = Math.sqrt(avgSquareDiff);
+      return stdDev;
+  }
+
+  function average(data){
+    // reduce iterates over every array value and produces a single result
+    // produces a sum of all the item in the array
+    var sum = data.reduce(function(sum, value){
+      return sum + value;
+    }, 0);
+    // divide the sum by the number of items in the array to get average
+    var avg = sum / data.length;
+    return avg;
+  }
+
 });
+
 </script>
 </body>
 </html>
